86c841a607dab83d5c284b63fbddedfa6f68c826
LFCKJ <k71689@naver.com>
2025-05-07 20:55:18 +0900

    onPress, Prompt구현현

--- 6기 김재환/script.js
+++ 6기 김재환/script.js
@@ -1,3 +1,33 @@
+class Calculator{
+    $PreviousPreviewPrompt="";
+    $CurrentpreviewPrompt="";
+    PreviousOperation ="";
+    CurrentOperation="";
+    constructor($PreviousPreview,$Currentpreview){
+        this.$PreviousPreviewPrompt = $PreviousPreview;
+        this.$CurrentpreviewPrompt = $Currentpreview;
+        
+    }
+
+    onPressNumber(number){
+        this.$CurrentpreviewPrompt.textContent += number;
+        
+    }
+    onPressOperation(operation){
+        console.log(operation);
+    }
+
+    onEqual(){}
+    onReset(){}
+    onDelete(){}
+
+    handlePlus(){}
+    handleminus(){}
+    handlemultiply(){}
+    handledivide(){}
+}
+
+
 // 연산자
 const $plus = document.querySelector("[data-btn-plus]");
 const $minus = document.querySelector("[data-btn-minus]");
@@ -10,23 +40,26 @@ const $eqaul = document.querySelector("[data-btn-eqaul]");
 const $reset = document.querySelector("[data-btn-reset]");
 const $delete = document.querySelector("[data-btn-delete]");
 
-//숫자,연산자자
+//숫자,연산자
 const $numbers = document.querySelectorAll("[data-btn-number]");
 const $operations = document.querySelectorAll("[data-btn-operation]");
-
+//프롬프트
 const $PreviousPreview = document.querySelector("[data-previous-preview]");
 const $Currentpreview = document.querySelector("[data-current-preview]");
 
 //숫자 선택
 $numbers.forEach(($number)=>{
     $number.addEventListener("click",(e)=>{
-        console.log(e.target);
+        calc.onPressNumber(e.target.textContent);
     });
 });
 
 //연산자선택
 $operations.forEach(($operation)=>{
     $operation.addEventListener("click",(e)=>{
-        console.log(e.target);
+       calc.onPressOperation($operation);
     });
 });
+
+// 객체 생성
+const calc = new Calculator($PreviousPreview, $Currentpreview);
