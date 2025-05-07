fa0c1a82a6f979cd9dbcf9625deb0658f84ade05
LFCKJ <k71689@naver.com>
2025-05-07 20:21:51 +0900

    DOM 작업업

---
+++ 6기 김재환/script.js
@@ -0,0 +1,32 @@
+// 연산자
+const $plus = document.querySelector("[data-btn-plus]");
+const $minus = document.querySelector("[data-btn-minus]");
+const $multiply = document.querySelector("[data-btn-mutiply]");
+const $divide = document.querySelector("[data-btn-divide]");
+const $eqaul = document.querySelector("[data-btn-eqaul]");
+
+//$-- 돔 변수이다== equalElement
+//AC,DEL
+const $reset = document.querySelector("[data-btn-reset]");
+const $delete = document.querySelector("[data-btn-delete]");
+
+//숫자,연산자자
+const $numbers = document.querySelectorAll("[data-btn-number]");
+const $operations = document.querySelectorAll("[data-btn-operation]");
+
+const $PreviousPreview = document.querySelector("[data-previous-preview]");
+const $Currentpreview = document.querySelector("[data-current-preview]");
+
+//숫자 선택
+$numbers.forEach(($number)=>{
+    $number.addEventListener("click",(e)=>{
+        console.log(e.target);
+    });
+});
+
+//연산자선택
+$operations.forEach(($operation)=>{
+    $operation.addEventListener("click",(e)=>{
+        console.log(e.target);
+    });
+});
